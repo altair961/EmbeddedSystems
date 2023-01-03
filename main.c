d58a0eb198a72ba88a28cf9b2a710921b2e96e09
altair961 <altair89@list.ru>
2023-01-03 21:22:16 +0300

    LED2 was on.

--- main.c
+++ main.c
@@ -12,4 +12,7 @@ void main(void)
 
     P1DIR |= 0x1;  // we add 0000 0001 to 0000 0000
     P1OUT |= 0x1; // we added 0000 0001 to 0000 0000
+
+    P1DIR |= 0x40; // we add 0100 0000
+    P1OUT |= 0x40; // we added 0100 0000
 }
