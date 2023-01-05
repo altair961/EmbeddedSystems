117972a204d4589bd4a7ac53d3b3b9bfdc65b8df
altair961 <altair89@list.ru>
2023-01-05 12:28:06 +0300

    LED2 was on.

--- LedsTurnedOn/main.c
+++ LedsTurnedOn/main.c
@@ -14,5 +14,8 @@ int main(void)
     P1DIR |= 0x1;  // we add 0000 0001 to 0000 0000
     P1OUT |= 0x1; // we added 0000 0001 to 0000 0000
 
+    P1DIR |= 0x40; // we add 0100 0000
+    P1OUT |= 0x40; // we added 0100 0000
+
 	return 0;
 }
